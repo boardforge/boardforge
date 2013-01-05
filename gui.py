8977a22d4dcd8b4acb37584889653b3c8c891327
boardforge <jeffmcalvay@boardforge.com>
2013-01-05 11:49:24 -0600

    Sebsauvage guide
    http://sebsauvage.net/python/gui/

--- gui.py
+++ gui.py
@@ -46,18 +46,46 @@ class simpleapp_tk(Tkinter.Tk):
 	def initialize(self):
 		self.grid()
 		
-		self.entry = Tkinter.Entry(self)
+		# Text box
+		self.entryVariable = Tkinter.StringVar()
+		self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
 		self.entry.grid(column=0,row=0,sticky='EW')
+		self.entry.bind("<Return>", self.OnPressEnter)
+		self.entryVariable.set(u"Enter text here.")
 		
-		button = Tkinter.Button(self,text=u"Button")
+		# Button
+		button = Tkinter.Button(self,text=u"Button",command=self.OnButtonClick)
 		button.grid(column=1,row=0)
 		
-		label = Tkinter.Label(self,anchor="w",fg="white",bg="blue",text=u"Label")
+		# Label
+		self.labelVariable = Tkinter.StringVar()
+		label = Tkinter.Label(self,textvariable=self.labelVariable,anchor="w",fg="white",bg="blue")
 		label.grid(column=0,row=1,columnspan=2,sticky='EW')
+		self.labelVariable.set(u"Hello!")
 		
+		# Manage resizing
 		self.grid_columnconfigure(0,weight=1)
 		self.resizable(True,False)
-
+		self.update()
+		self.geometry(self.geometry())
+		
+		# Focus on text box
+		self.entry.focus_set()
+		self.entry.selection_range(0,Tkinter.END)
+		
+	def OnButtonClick(self):
+		self.labelVariable.set(self.entryVariable.get()+" You clicked the button!")
+		
+		# Focus on text box
+		self.entry.focus_set()
+		self.entry.selection_range(0,Tkinter.END)
+		
+	def OnPressEnter(self,event):
+		self.labelVariable.set(self.entryVariable.get()+" You pressed enter!")
+		
+		# Focus on text box		
+		self.entry.focus_set()
+		self.entry.selection_range(0,Tkinter.END)
 if __name__ == "__main__":
 	app = simpleapp_tk(None)
 	app.title('Board Forge - www.boardforge.com')
